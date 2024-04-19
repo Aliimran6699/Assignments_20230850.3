d63b2680476edbb65d9d2105be4a7d91a205d2d1
Aliimran6699 <162525085+Aliimran6699@users.noreply.github.com>
2024-04-19 13:18:06 +1200

    Update floatGrade.py

--- LABex.py
+++ LABex.py
@@ -1,9 +1,11 @@
-M1 = float(input("enter M1:"))
-M2 = float(input("enter M2:"))
-M3 = float(input("enter M3:"))
-M4 = float(input("enter M4:"))
-Grade= M1+M2+M3+M4/4
-if (Grade<50):
-    print("fail")
+M1 = float(input("Enter M1: "))
+M2 = float(input("Enter M2: "))
+M3 = float(input("Enter M3: "))
+M4 = float(input("Enter M4: "))
+
+Grade = (M1 + M2 + M3 + M4) / 4
+
+if Grade < 50:
+    print("Fail")
 else:
-    print("pass")
+    print("Pass")
